e4e5662d99491ae429569f7acabd336608c68613
Edwing Ruiz Sanchez <edwing9@gmail.com>
2021-06-29 14:43:19 -0500

    test numbers todas las pruebas han pasado

--- __test__/numbers.test.js
+++ __test__/numbers.test.js
@@ -2,18 +2,18 @@ import { numbers } from '../numbers';
 
 describe('Comparación de numeros', () => {
     test('Mayor que', () => {
-        expect(numbers(2, 2)).toBeGreaterThan(6);
+        expect(numbers(2, 2)).toBeGreaterThan(3);
     });
     test('Mayor que o igual', () => {
-        expect(numbers(2, 2)).toBeGreaterThanOrEqual(5);
+        expect(numbers(2, 2)).toBeGreaterThanOrEqual(4);
     });
     test('Menor que', () => {
-        expect(numbers(2, 2)).toBeLessThan(2);
+        expect(numbers(2, 2)).toBeLessThan(5);
     });
     test('Menor que o igual', () => {
-        expect(numbers(2, 2)).toBeLessThanOrEqual(1);
+        expect(numbers(2, 2)).toBeLessThanOrEqual(4);
     });
     test('Numeros flotantes', () => {
-        expect(numbers(2.2, 2.2)).toBeCloseTo(4.0);
+        expect(numbers(2.2, 2.2)).toBeCloseTo(4.4);
     })
 })
