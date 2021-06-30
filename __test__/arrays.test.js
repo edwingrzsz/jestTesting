6451de7035235fda83e9fdd433d46ac2f9f6c285
Edwing Ruiz Sanchez <edwing9@gmail.com>
2021-06-30 12:43:36 -0500

    test de un arreglo con jest toContain, not.toContain, toHaveLength

---
+++ __test__/arrays.test.js
@@ -0,0 +1,13 @@
+import { arrayFruits, arrayColors } from "../arrays";
+
+describe('Comprobaremos que existe un elemento', () => {
+    test('¿Tiene una banana?', () => {
+        expect(arrayFruits()).toContain('Banana');
+    });
+    test('No contiene un platano', () => {
+        expect(arrayFruits()).not.toContain('Platano');
+    });
+    test('Comprobar el tamaño de un arreglo', () => {
+        expect(arrayFruits()).toHaveLength(5);
+    });
+});
---
+++ arrays.js
@@ -0,0 +1,5 @@
+const fruits = ['Banana', 'Melon', 'Sandia', 'Naranja', 'Limón', 'Pepino'];
+const colors = ['azul', 'verde', 'rojo', 'rosa', 'amarillo'];
+
+export const arrayFruits = () => fruits;
+export const arrayColors = () => colors;
